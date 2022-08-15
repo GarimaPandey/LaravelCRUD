f859a956c1ac04ed533115ebe8ce19a12a686f3c
GARIMA PANDEY <pandeygarima704@gmail.com>
2022-08-15 04:13:00 -0700

    model created for posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'title',
+        'url',
+        'description',
+    ];
+
+    
 }
